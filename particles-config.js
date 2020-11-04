2171453c749998d673423681e6d096776c9a6467
Amethy <73916897+Amathysto@users.noreply.github.com>
2020-11-04 05:18:02 +0000

    a

--- particles-config.js
+++ particles-config.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
   particles: {
     number: { value: 80, density: { enable: true, value_area: 800 } },
-    color: { value: "#ffffff" },
+    color: { value: "#32CD32" },
     shape: {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
@@ -9,7 +9,7 @@ particlesJS("particles-js", {
       image: { src: "img/github.svg", width: 100, height: 100 }
     },
     opacity: {
-      value: 0.5,
+      value: 0.6,
       random: false,
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
     },
@@ -21,7 +21,7 @@ particlesJS("particles-js", {
     line_linked: {
       enable: true,
       distance: 150,
-      color: "#ffffff",
+      color: "#7CFC00",
       opacity: 0.4,
       width: 1
     },
