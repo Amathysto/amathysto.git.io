4e658e1b3b12759cad32147364efa490e91b95da
Amethy <73916897+Amathysto@users.noreply.github.com>
2021-04-25 02:40:53 +0100

    It's a summary.
    yes.
    
    Removed .html at the end of redirects
    Added tamper protection
    (potentially a moving title, maybe.)

---
+++ code/title.js
@@ -0,0 +1,34 @@
+/* 
+
+-- Copyright (C) Fallen-Network, Inc - All Rights Reserved
+-- Unauthorized copying of this file, via any medium is strictly prohibited
+-- Proprietary and confidential
+-- Written by Damien <https://github.com/Damien111>, November 2020
+
+*/
+
+if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", function() {
+        loaded();
+    });
+} else if (document.attachEvent) {
+    document.attachEvent("onreadystatechange", function() {
+        loaded();
+    });
+}
+
+function loaded() {
+
+    setInterval(loop, 600);
+
+}
+
+var x = 0;
+
+var titleText = ["C", "Cr", "Cry", "Crys", "Cryst", "Crysta" , "Crystal", "Crystal S", "Crystal Sq", "Crystal Squ", "Crystal Squa", "Crystal Squad"];
+
+function loop() {
+
+    document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
